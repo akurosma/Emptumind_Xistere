b9bab60b982e24fa6ab6e6daecfc8d54c82e3a71
akuro <akuro_sma>
2024-12-18 23:47:24 +0900

    LoveHeart and lighting effect

---
+++ actors/star/geo_header.h
@@ -0,0 +1,11 @@
+extern const GeoLayout star_geo[];
+extern Lights1 star__11_f3d_001_lights;
+extern Lights1 star__1_f3d_lights;
+extern u8 star__1_rgba16[];
+extern Vtx star_LoveHeart_mesh_layer_1_vtx_0[192];
+extern Gfx star_LoveHeart_mesh_layer_1_tri_0[];
+extern Vtx star_LoveHeart_mesh_layer_1_vtx_1[202];
+extern Gfx star_LoveHeart_mesh_layer_1_tri_1[];
+extern Gfx mat_star__11_f3d_001[];
+extern Gfx mat_star__1_f3d[];
+extern Gfx star_LoveHeart_mesh_layer_1[];
--- src/game/obj_behaviors.c
+++ src/game/obj_behaviors.c
@@ -774,3 +774,6 @@ UNUSED s32 debug_sequence_tracker(s16 debugInputSequence[]) {
 #include "behaviors/treasure_chest.inc.c"
 #include "behaviors/mips.inc.c"
 #include "behaviors/yoshi.inc.c"
+
+
+#include "behaviors/omm.inc.c"
