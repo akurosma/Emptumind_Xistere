6205c4af509d798598162cf75721f95e454dcd45
sill <sill@example.com>
2026-01-07 06:28:42 +0900

    2月から本気出す。特に変更なし

--- levels/hmc/texscroll.inc.c
+++ levels/hmc/texscroll.inc.c
@@ -1,13 +1,34 @@
-void scroll_hmc_dl_TIE_Area1_v3_6_mesh_layer_1_vtx_24() {
+void scroll_hmc_dl__C6_cloud1_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 56;
-	int width = 32 * 0x20;
+	int count = 849;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(hmc_dl__C6_cloud1_mesh_layer_5_vtx_0);
+
+	deltaY = (int)(0.30000001192092896 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_hmc_dl__C6_cloud2_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 2349;
+	int width = 64 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
-	Vtx *vertices = segmented_to_virtual(hmc_dl_TIE_Area1_v3_6_mesh_layer_1_vtx_24);
+	Vtx *vertices = segmented_to_virtual(hmc_dl__C6_cloud2_mesh_layer_1_vtx_0);
 
-	deltaX = (int)(0.009999999776482582 * 0x20) % width;
+	deltaX = (int)(1.0 * 0x20) % width;
 
 	if (absi(currentX) > width) {
 		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
@@ -19,16 +40,59 @@ void scroll_hmc_dl_TIE_Area1_v3_6_mesh_layer_1_vtx_24() {
 	currentX += deltaX;
 }
 
-void scroll_gfx_mat_hmc_dl__09_f3d_layer1() {
-	Gfx *mat = segmented_to_virtual(mat_hmc_dl__09_f3d_layer1);
+void scroll_hmc_dl__C6_main_mesh_layer_5_vtx_1() {
+	int i = 0;
+	int count = 4;
+	int height = 64 * 0x20;
 
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(hmc_dl__C6_main_mesh_layer_5_vtx_1);
 
-	shift_s(mat, 12, PACK_TILESIZE(0, 1));
-	shift_t(mat, 17, PACK_TILESIZE(0, 1));
+	deltaY = (int)(-1.0 * 0x20) % height;
 
-};
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_hmc_dl__C6_main_mesh_layer_5_vtx_2() {
+	int i = 0;
+	int count = 22;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(hmc_dl__C6_main_mesh_layer_5_vtx_2);
+
+	deltaX = (int)(0.5 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;	currentY += deltaY;
+}
 
 void scroll_hmc() {
-	scroll_hmc_dl_TIE_Area1_v3_6_mesh_layer_1_vtx_24();
-	scroll_gfx_mat_hmc_dl__09_f3d_layer1();
+	scroll_hmc_dl__C6_cloud1_mesh_layer_5_vtx_0();
+	scroll_hmc_dl__C6_cloud2_mesh_layer_1_vtx_0();
+	scroll_hmc_dl__C6_main_mesh_layer_5_vtx_1();
+	scroll_hmc_dl__C6_main_mesh_layer_5_vtx_2();
 };
